c7dd8a6f684f161245184f10458bdc0b6f7afb05
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-03-17 20:34:11 -0700

    adding ids to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+
+// main form
+city-form
+
+ //past city searches
+cityButtons
+
+//today weather container
+weather-today
+
+      
+city-search
+todayDate
+todayIcon
+todayTemp
+todayHumidity
+todayWind
+todayUV
+
+//Forecast 5 container
+forecast5day
+  
+day1
+icon1
+temperature1
+humidity1 
+  
+day2
+icon2
+temperature2
+humidity2
+             
+day3
+icon3
+temperature3
+humidity3
+              
+
+day4
+icon4
+temperature4
+humidity4
+
+day5
+icon5
+temperature5
+humidity5
+                     
